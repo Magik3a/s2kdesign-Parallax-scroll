8813753cded75d6a3f12160c317273b8fb86686d
Magik3a <svetlin.krastanov90@gmail.com>
2015-06-19 09:54:40 +0300

    add files
    2 pages is finished. Left to make something about video page

---
+++ SiteScrollDownEffect/js/changepage.js
@@ -0,0 +1,28 @@
+﻿  
+$(function () {
+    "use strict";
+    $("#news").click(function () {
+        $("#pages").hide(1000, function () {
+            $("#pages").load("article.html");
+            $("#pages").show(1000);
+        });
+    });
+    $("#clips").click(function () {
+        $("#pages").hide(1000, function () {
+            $("#pages").load("clips.html");
+            $("#pages").show(1000);
+        });
+    });
+    $("#books").click(function () {
+        $("#pages").hide(1000, function () {
+            $("#pages").load("books.html");
+            $("#pages").show(1000);
+        });
+    });
+    $("#links").click(function () {
+        $("#pages").hide(1000, function () {
+            $("#pages").load("links.html");
+            $("#pages").show(1000);
+        });
+    });
+});
